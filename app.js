8ee5e9a16a31309d149b6994ffe7183d031c281c
Akhil Varma <akhilvarma423@gmail.com>
2018-03-26 12:46:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 
+//url validation
+
+var key; 
+var url='url_of_website/key';
+
+(function(){
+
+var http= new XMLHttprequest();
+  
+  http.onreadystateexchange = function(req ,res ){
+  
+     if(this.readyState==4 && this.status==200){
+     
+          if(this.responseText==true)
+            key=true;
+          else
+            key=false;
+    
+     
+     }
+    
+    
+    http.open('GET',url,true);
+    http.send();
+  
+  }
+
+})();
+
+
+// if the url is valid then we go for registration 
+if(key==true){
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+    console.log('Yey!', reg);
+  }).catch(function(err) {
+    console.log('Boo!', err);
+  });
+  
+}
+  
+}
