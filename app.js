3576617054fff3fc1762a665432cd3fe504edf73
Akhil Varma <akhilvarma423@gmail.com>
2018-03-26 15:57:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var http= new XMLHttprequest();
 //if(key==true){
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+  navigator.serviceWorker.register('sw.js').then(function(reg) {
     console.log('Yey!', reg);
   }).catch(function(err) {
     console.log('Boo!', err);
